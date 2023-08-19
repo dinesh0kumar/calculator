31d4055784e618b320f3492d620a827ee71423da
Dinesh Kumar <dk9671399889@gmail.com>
2023-08-19 14:15:20 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './footer.css'
+export default function Footer() {
+    var stringCode = "</>"
+    return (
+        <footer>
+            <p> {stringCode} by: <a href="https://github.com/dinesh0kumar">Dinesh Kumar</a> </p>
+            <p><a rel="noreferrer" target="_blank" href="https://github.com/dinesh0kumar">github</a></p>
+        </footer>
+    )
+}
